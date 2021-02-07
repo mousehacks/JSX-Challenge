fd7bfe442471b5a3fbb4d325ea39c227c0df153e
mousehacks <77026371+mousehacks@users.noreply.github.com>
2021-02-07 15:48:01 +0100

    Initial commit

--- src/index.js
+++ src/index.js
@@ -2,3 +2,17 @@
 //It should display a h1 heading.
 //It should display an unordered list (bullet points).
 //It should contain 3 list elements.
+import React from "react";
+import ReactDOM from "react-dom";
+
+ReactDOM.render(
+  <div>
+    <h1>Hello World</h1>
+    <ul>
+      <li>Point 1</li>
+      <li>Point 2</li>
+      <li>Point 3</li>
+    </ul>
+  </div>,
+  document.getElementById("root")
+);
